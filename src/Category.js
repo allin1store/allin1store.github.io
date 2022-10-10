05d5fe2d342d8e58f873aa23c89f9b1188f90b05
sboioi <64840151+sboioi@users.noreply.github.com>
2022-10-10 11:28:45 -0700

    Create Category.js

---
+++ src/Category.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// to do:
+// 1. display a specific category (9 random items in a page)
+//    a. picture and name, link to the prouct
+//    b. pagination at the top and the bottom
+//    c. "show more" button at the bottom
+
+const Category = props => {
+
+    return (
+        <div className="wrapper">
+            <main>
+            <section id="category">
+                <div className="text">
+                    <h2>Category Page</h2>
+                    
+                </div>
+            </section>
+        </main>
+        <footer>&nbsp;</footer>
+        </div>
+        )
+};
+
+export default Category;
